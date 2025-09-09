ca27fe09027a8fd03ad3253ad441df4ad14a65d4
krishna-mouli <moulimorampudi@github.com>
2025-09-09 21:54:15 +0530

    Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,3 @@
-// import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-// import TodoPage from "./pages/TodoPage";
-// import LoginPage from "./pages/LoginPage";
-// import SignupPage from "./pages/SignupPage";
-// import LandingPage from "./pages/LandingPage";
-// import Custom404Page from "./pages/Custom404Page";
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/todos" element={<TodoPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/signup" element={<SignupPage />} />
-//         <Route path="*" element={<Custom404Page />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-
-
-
-
-
-
-
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { todoApi } from './api/todoApi';
